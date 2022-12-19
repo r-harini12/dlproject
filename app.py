07d5642ab11378c028dbfde01b2390244defbed2
r-harini12 <96470500+r-harini12@users.noreply.github.com>
2022-12-19 22:08:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from chat import get_response
 app = Flask(__name__)
 @app.get("/")
 def index_get():
-    return render_template("base.html")
+    return render_template("static/base.html")
 
 @app.post("/predict")
 def predict():
